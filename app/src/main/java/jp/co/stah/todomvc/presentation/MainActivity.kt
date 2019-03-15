002494bee5679f1b92a1d4f007a6c2552e258516
yoshida0261 <yoshida261@gmail.com>
2019-03-16 01:39:03 +0900

    下タブを実装

--- app/src/main/java/jp/co/stah/todomvc/presentation/MainActivity.kt
+++ app/src/main/java/jp/co/stah/todomvc/presentation/MainActivity.kt
@@ -1,18 +1,40 @@
 package jp.co.stah.todomvc.presentation
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.widget.ListView
 import jp.co.stah.todomvc.R
 import jp.co.stah.todomvc.presentation.todo.TodoListAdapter
 import jp.co.stah.todomvc.presentation.todo.TodoListItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     val items = ArrayList<TodoListItem>()
 
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_all -> {
+                //message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_active -> {
+                //message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_check -> {
+                //message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val list = findViewById<ListView>(R.id.todo_list)
         val data = TodoListItem(false, "テスト")
